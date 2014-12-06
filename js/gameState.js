4b413d6948bec008160cc8fb35f2997865db667f
Taylor Flagg <trflagg@gmail.com>
2014-12-06 14:42:36 -0500

    add pedestrian

--- js/gameState.js
+++ js/gameState.js
@@ -8,6 +8,7 @@ define(['phaser'
 
   GameState.prototype.preload = function() {
     this.load.image('platform', 'img/platform.png');
+    this.load.image('pedestrian', 'img/pedestrian.png');
   };
 
   GameState.prototype.create = function() {
@@ -17,8 +18,10 @@ define(['phaser'
     this.game.scale.pageAlignHorizontally = true;
     this.game.debug.geom(new Phaser.Rectangle(0, 0, this.game.width - 1, this.game.height), '#ffff00', false);
 
-    this.add.existing(new Platform(this.game, 0, 100));
-    this.add.existing(new Platform(this.game, 128, 100));
+    this.add.existing(new Platform(this.game, 0, 128));
+    this.add.existing(new Platform(this.game, 128, 128));
+
+    this.add.existing(new Phaser.Sprite(this.game, 0, 0, 'pedestrian'));
   }
   return GameState;
 })
