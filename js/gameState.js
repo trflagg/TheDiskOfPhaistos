3c550987f7c9abeb3849cae80d178804fd5101f6
Taylor Flagg <trflagg@gmail.com>
2014-12-06 17:11:03 -0500

    Click does simple jump.

--- js/gameState.js
+++ js/gameState.js
@@ -24,19 +24,21 @@ define(['phaser'
     this.game.debug.geom(new Phaser.Rectangle(0, 0, this.game.width - 1, this.game.height), '#ffff00', false);
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.physics.arcade.gravity.y = 300;
+    this.game.physics.arcade.gravity.y = 1000;
 
     // add controls
     this.left = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
     this.right = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
 
+    this.input.onDown.add(this.click, this);
+
     // make screen array
     this.screen = Screen(this);
 
     var pedestrian = this.add.existing(new Phaser.Sprite(this.game, 0, 0, 'pedestrian'));
-    pedestrian.scale = new Phaser.Point(this.sprite_scale_factor, this.sprite_scale_factor);
+    pedestrian.scale = new Phaser.Point(this.sprite_scale_factor / 2, this.sprite_scale_factor / 2);
     this.game.physics.enable(pedestrian, Phaser.Physics.ARCADE);
-    pedestrian.body.gravity.y = 1000;
+    // pedestrian.body.gravity.y = 1000;
     pedestrian.body.maxVelocity.y = 500;
     pedestrian.body.collideWorldBounds = true;
 
@@ -55,6 +57,10 @@ define(['phaser'
     }
     this.game.physics.arcade.collide(this.pedestrian, this.screen.platforms);
   }
+
+  GameState.prototype.click = function() {
+    this.pedestrian.body.velocity.y = -400;
+  };
 
   return GameState;
 })
