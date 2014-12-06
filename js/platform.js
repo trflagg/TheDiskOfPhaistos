f90edd2b639638682a10651be20c789b5086dccc
Taylor Flagg <trflagg@gmail.com>
2014-12-06 18:29:32 -0500

    platforms start deactivated and can be activated

--- js/platform.js
+++ js/platform.js
@@ -1,14 +1,23 @@
 define(['phaser'], function(phaser) {
 
-  var Platform = function(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'platform');
-    game.add.existing(this);
-    game.physics.enable(this, Phaser.Physics.ARCADE);
+  var Platform = function(state, x, y) {
+    Phaser.Sprite.call(this, state.game, x, y, 'platform');
+    this.state = state;
+    state.game.add.existing(this);
+    state.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.immovable = true;
     this.body.allowGravity = false;
+
+    this.visible = false;
   }
   Platform.prototype = Object.create(Phaser.Sprite.prototype);
   Platform.prototype.constructor = Platform;
+
+  Platform.prototype.activate = function(screen) {
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+    screen.platforms.add(this);
+    this.visible = true;
+  };
 
   return Platform;
 
