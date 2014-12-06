0cb68dee070951a82ce8a5bfcd4cd888d969dbed
Taylor Flagg <trflagg@gmail.com>
2014-12-06 15:26:36 -0500

    Move start down 64px

--- js/gameState.js
+++ js/gameState.js
@@ -26,7 +26,7 @@ define(['phaser'
     // make screen array
     this.screen = Screen(this);
 
-    this.pedestrian = this.add.existing(new Phaser.Sprite(this.game, 0, 0, 'pedestrian'));
+    this.pedestrian = this.add.existing(new Phaser.Sprite(this.game, 0, 64, 'pedestrian'));
     this.pedestrian.scale = new Phaser.Point(this.sprite_scale_factor, this.sprite_scale_factor);
   }
   return GameState;
--- js/screen.js
+++ js/screen.js
@@ -8,9 +8,9 @@ define(['phaser'
 
     // screen[0]
     screen[0] = [];
-    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 64 * state.position_scale_factor));
+    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 128 * state.position_scale_factor));
     screen[0][0].scale = new Phaser.Point(state.sprite_scale_factor, state.sprite_scale_factor);
-    screen[0][1] = state.game.add.existing(new Platform(state.game, 64 * state.position_scale_factor, 64 * state.position_scale_factor));
+    screen[0][1] = state.game.add.existing(new Platform(state.game, 64 * state.position_scale_factor, 128 * state.position_scale_factor));
     screen[0][1].scale = new Phaser.Point(state.sprite_scale_factor, state.sprite_scale_factor);
 
     return screen;
