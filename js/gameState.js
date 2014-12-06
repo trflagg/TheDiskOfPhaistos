d1515f7ceeffbb8a647c189d42bf3fd3df780131
Taylor Flagg <trflagg@gmail.com>
2014-12-06 15:20:21 -0500

    Use state.scale_factor

--- js/gameState.js
+++ js/gameState.js
@@ -17,12 +17,16 @@ define(['phaser'
     this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     this.game.scale.pageAlignVertically = true;
     this.game.scale.pageAlignHorizontally = true;
+
+    this.scale_factor = 0.5;
+
     this.game.debug.geom(new Phaser.Rectangle(0, 0, this.game.width - 1, this.game.height), '#ffff00', false);
 
     // make screen array
     this.screen = Screen(this);
 
     this.pedestrian = this.add.existing(new Phaser.Sprite(this.game, 0, 0, 'pedestrian'));
+    this.pedestrian.scale = new Phaser.Point(this.scale_factor, this.scale_factor);
   }
   return GameState;
 })
--- js/screen.js
+++ js/screen.js
@@ -8,8 +8,10 @@ define(['phaser'
 
     // screen[0]
     screen[0] = [];
-    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 128));
-    screen[0][1] = state.game.add.existing(new Platform(state.game, 128, 128));
+    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 128 * state.scale_factor));
+    screen[0][0].scale = new Phaser.Point(state.scale_factor, state.scale_factor);
+    screen[0][1] = state.game.add.existing(new Platform(state.game, 128 * state.scale_factor, 128 * state.scale_factor));
+    screen[0][1].scale = new Phaser.Point(state.scale_factor, state.scale_factor);
 
     return screen;
   }
