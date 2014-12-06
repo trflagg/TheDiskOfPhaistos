ee0fb1bf92baa89f740cc587250e40e63baafe9d
Taylor Flagg <trflagg@gmail.com>
2014-12-06 15:24:30 -0500

    Differentiate sprite_scale_factor and position_scale_factor

--- js/screen.js
+++ js/screen.js
@@ -8,10 +8,10 @@ define(['phaser'
 
     // screen[0]
     screen[0] = [];
-    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 128 * state.scale_factor));
-    screen[0][0].scale = new Phaser.Point(state.scale_factor, state.scale_factor);
-    screen[0][1] = state.game.add.existing(new Platform(state.game, 128 * state.scale_factor, 128 * state.scale_factor));
-    screen[0][1].scale = new Phaser.Point(state.scale_factor, state.scale_factor);
+    screen[0][0] = state.game.add.existing(new Platform(state.game, 0, 64 * state.position_scale_factor));
+    screen[0][0].scale = new Phaser.Point(state.sprite_scale_factor, state.sprite_scale_factor);
+    screen[0][1] = state.game.add.existing(new Platform(state.game, 64 * state.position_scale_factor, 64 * state.position_scale_factor));
+    screen[0][1].scale = new Phaser.Point(state.sprite_scale_factor, state.sprite_scale_factor);
 
     return screen;
   }
